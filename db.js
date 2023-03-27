388c87b502e2aa6c5f1bad12770525612a0f3a70
palabhi017 <palabhi017@gmail.com>
2023-03-27 21:34:46 +0530

    daf

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 const connection = async()=>{
  try {
-   await mongoose.connect(process.env.dburl)
+   await mongoose.connect("mongodb+srv://abhishek:abhishekpal@cluster0.tbti2yd.mongodb.net/linkedinDB?retryWrites=true&w=majority")
    console.log("connected")
  } catch (error) {
     console.log(error)
