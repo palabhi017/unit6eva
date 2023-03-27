eaa12184e39cbebce184c34fc677a3fad50db390
palabhi017 <palabhi017@gmail.com>
2023-03-27 21:29:10 +0530

    daf

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 const connection = async()=>{
  try {
-   await mongoose.connect(process.env.mongoURL)
+   await mongoose.connect(process.env.dburl)
    console.log("connected")
  } catch (error) {
     console.log(error)
